8d6eddf9781fdb9c5a8a76958532d1441bdbc7c0
Dylan Rice <2769579r@student.gla.ac.uk>
2025-01-23 23:01:54 +0000

    Populate database and customise admin page

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['name']}),
+        ('Metrics', {'fields': ['views', 'likes']}),
+    ]
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+    fieldsets = [
+        (None,      {'fields': ['title', 'category', 'url']}),
+        ('Metrics', {'fields': ['views']}),
+    ]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
